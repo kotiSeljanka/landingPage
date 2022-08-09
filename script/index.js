e0d77314b17b33c6e6650ba0650e7512c30c3933
Aleksei Pankov <aleksei.pankov.oku@gmail.com>
2022-08-09 11:06:48 +0300

    my work, dynamic css, content

--- script/index.js
+++ script/index.js
@@ -2,6 +2,6 @@ const toTopButton = document.getElementById("toTop");
 
 let prevScrollPos = window.scrollY;
 window.onscroll = function() {
-    (prevScrollPos > window.scrollY ? toTopButton.style.opacity = "0" : toTopButton.style.opacity = "1");
+    (prevScrollPos > window.scrollY ? toTopButton.style.opacity = "1" : toTopButton.style.opacity = "0");
     prevScrollPos = window.scrollY;
 }
