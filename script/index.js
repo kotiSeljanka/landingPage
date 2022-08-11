116988860d305dd585dc49243f0677f410ade757
Aleksei Pankov <aleksei.pankov.oku@gmail.com>
2022-08-11 15:45:06 +0300

    pad and desktop views

--- script/index.js
+++ script/index.js
@@ -1,7 +1,16 @@
 const toTopButton = document.getElementById("toTop");
 
+const menuButtonOuter = document.getElementById("menuButtonOuter");
+const menuButtonInner = document.getElementById("menuButtonInner")
+const padMenu = document.getElementById("navMobile");
+
 let prevScrollPos = window.scrollY;
 window.onscroll = function() {
     (prevScrollPos > window.scrollY ? toTopButton.style.opacity = "1" : toTopButton.style.opacity = "0");
     prevScrollPos = window.scrollY;
+}
+
+function togglePadMenu() {
+    padMenu.classList.toggle("padVisible");
+    menuButtonInner.classList.toggle("padArrowVisible");
 }
